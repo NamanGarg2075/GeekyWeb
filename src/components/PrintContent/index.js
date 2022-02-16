f70199246cccf6173e8208a4a1d4dcf66af5cff3
Designatory <namanehsaas@gmail.com>
2022-02-16 21:47:46 +0530

    Social Container removed

--- src/components/PrintContent/index.js
+++ src/components/PrintContent/index.js
@@ -1,7 +1,4 @@
 import React from 'react';
-
-import   './main'
-// import   '@site/src/components/PrintContenr'
 // import Layout from '@theme/Layout';
 // import styles from '../pages/introduction.css'
 // import './courses.module.css' // if needed then
@@ -9,19 +6,13 @@ import   './main'
 // import Link from '@docusaurus/Link';
 
 function socialShare() {
-    // function printContent() {
-    //     window.print()
-    // }
+    function printContent() {
+        window.print()
+    }
     return (
         <>
-    {/* <button class="button button--primary" onClick={printContent}>🖨️ Print</button> */}
+    <button class="button button--primary" onClick={printContent}>🖨️ Print</button>
     
-    <div class="a2a_kit a2a_kit_size_32 a2a_default_style">
-<a class="a2a_dd" href="https://www.addtoany.com/share"></a>
-<a class="a2a_button_facebook"></a>
-<a class="a2a_button_twitter"></a>
-<a class="a2a_button_email"></a>
-</div>
     </>
   );
 }
