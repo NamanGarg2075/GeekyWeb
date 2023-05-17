a70d991329fbf93df37ce7e40557330b82c1de6f
Mutasim <qmutasim77@gmail.com>
2023-05-17 16:51:23 +0600

    Updated

--- src/components/BrowserWindow/index.tsx
+++ src/components/BrowserWindow/index.tsx
@@ -1,4 +1,4 @@
-import React, {type ReactNode} from 'react';
+import React, { type ReactNode } from 'react';
 
 import './styles.index.css';
 
@@ -14,12 +14,12 @@ function BrowserWindow({
   url = 'GeekyWeb',
 }: Props): JSX.Element {
   return (
-    <div className="browserWindow" style={{minHeight}}>
+    <div className="browserWindow" style={{ minHeight }}>
       <div className="browserWindowHeader">
         <div className="buttons">
-          <span className="dot" style={{background: '#f25f58'}} />
-          <span className="dot" style={{background: '#fbbe3c'}} />
-          <span className="dot" style={{background: '#58cb42'}} />
+          <span className="dot" style={{ background: '#f25f58' }} />
+          <span className="dot" style={{ background: '#fbbe3c' }} />
+          <span className="dot" style={{ background: '#58cb42' }} />
         </div>
         <div className="browserWindowAddressBar">{url}</div>
         <div className="browserWindowMenuIcon">
